dc76a0ca9a1d2e781096720e2aefbc1d3b98bb01
Leonardo Contini <leonardo.contini@outlook.com>
2021-12-20 15:27:45 +0000

    ajusta teste ami

--- 02-pipeline-AMI/terraform-ami/main.tf
+++ 02-pipeline-AMI/terraform-ami/main.tf
@@ -1,6 +1,11 @@
 resource "aws_ami_from_instance" "template-ami" {
   name               = "template-ami-${var.versao}"
   source_instance_id = var.resource_id
+
+  tags = {
+    Name = "template-ami-${var.versao}"
+    Group = "Grupo7"
+  }
 }
 
 output "ami" {
