101a895f1cf4248fdcacd886e731e6dbe968664d
Leonardo Contini <leonardo.contini@outlook.com>
2021-12-20 15:14:23 +0000

    adiciona -no-color nas execucoes do terraform para deixar a saida mais friendly no output do jenkins 2

--- 04-deploy-mysql/destroy.sh
+++ 04-deploy-mysql/destroy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ./terraform
-terraform init
-TF_VAR_ami_id=$(terraform output ami_id) terraform destroy -auto-approve
+terraform init -no-color
+TF_VAR_ami_id=$(terraform output ami_id) terraform destroy -auto-approve -no-color
